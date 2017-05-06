463a9a87dafecd397457fca8db9f6a18e74d72d1
Meng Shi <mengshi@meicai.cn>
2017-05-06 12:25:07 +0800

    first

---
+++ outputs/stdout/src/stdout_module.go
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Meng Shi
+ */
+
+package stdout
+
+import (
+      "unsafe"
+    . "github.com/rookie-xy/main/types"
+)
+
+const (
+    STDOUT_MODULE = OUTPUT_MODULE|0x01000000
+    STDOUT_CONFIG = USER_CONFIG|CONFIG_ARRAY
+)
+
+type Stdout struct{
+    *Module
+}
+
+var stdout = String{ len("stdout"), "stdout" }
+var stdoutCommands = []Command{
+
+    { stdout,
+      STDOUT_CONFIG,
+      stdoutBlock,
+      0,
+      0,
+      nil },
+
+    NilCommand,
+}
+
+func stdoutBlock(c *Configure, _ *Command, _ *unsafe.Pointer) int {
+    if nil == c {
+        return Error
+    }
+
+    flag := STDOUT_CONFIG|CONFIG_VALUE
+    Block(c, Modulers, STDOUT_MODULE, flag)
+
+    return Ok
+}
+
+var stdoutModule = &Module{
+    MODULE_V1,
+    CONTEXT_V1,
+    nil,
+    stdoutCommands,
+    OUTPUT_MODULE,
+}
+
+func init() {
+    Modulers = Load(Modulers, &Stdout{stdoutModule})
+}
